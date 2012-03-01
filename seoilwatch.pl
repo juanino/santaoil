f312520287abbda1e3c9ae404cbb5c6ae746eafe
Jerry Vines <root@kidsnet.(none)>
2012-02-29 22:00:24 -0500

    added yml. added high and low alert

--- seoilwatch.pl
+++ seoilwatch.pl
@@ -3,9 +3,21 @@
 use strict;
 use WWW::Mechanize;
 use Data::Dumper;
+use YAML::Tiny;
 
-my $login = "XXXXXX";
-my $password = "XXXXXXXX";
+my $config_location = "/etc/seoilwatch.yml";
+my $yaml = YAML::Tiny->new;
+$yaml = YAML::Tiny->read($config_location);
+unless ($yaml) {
+   print "Failed to load config from $config_location \n";
+   exit;
+}
+
+my $login = $yaml->[0]->{login};
+my $password = $yaml->[0]->{password};
+my $highmark = $yaml->[0]->{highmark};
+my $lowmark = $yaml->[0]->{lowmark};
+my $alertemail = $yaml->[0]->{alertemail};
 
 my $url = "https://info.santaenergy.com/";
 
@@ -27,6 +39,18 @@ my $near=0;
 foreach my $line (@lines) {
     if ($near) {
        print "Daily price -> " .  &format($line);
+       my $dailyprice = &format($line);
+       $dailyprice =~ s/\$//g;
+       if ($dailyprice > $highmark) {
+           print " -ALERT- Daily price is over highmark.";
+           my $cmd = "/bin/echo oil over highmark at $dailyprice | mail $alertemail";
+           `$cmd`;
+       }
+       if ($dailyprice < $lowmark) {
+           print " -ALERT- Daily price under lowmark.";
+           my $cmd = "/bin/echo oil under lowmark at $dailyprice | mail $alertemail";
+           `$cmd`;
+       }
        $near=0;
     }
     if ($line =~ /Santa Daily Price/) {
